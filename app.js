7c414c721aef8897c63a0873a477fcf9453d495d
Donkey <danglayers@gmail.com>
2020-07-21 19:57:09 -0400

    began work on application

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const team = [];
+
+const newMember = {
+    name: "continue",
+    type: "confirm",
+    message: "Add a new member?",
+};
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
